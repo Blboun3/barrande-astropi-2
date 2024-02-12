8bf99d31e4458ff65731221f79cfc7e9098a3ae3
yeesaurus <cokolicokolicokoli@gmail.com>
2024-02-12 23:16:00 +0100

    a little bit of main.py

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-print("red sus")
+from photo_creation import photographer
+import time
+
+#       start of the program
+
+#   variable declaration
+
+# variables for time keeping
+time_start = time.time()
+time_end = (time.time() + 570)
+mamecas = True
+
+# file name numbering
+photo_number = 0
+picforcomp_1 = None
+picforcomp_2 = None
+
+# variable debugging
+print(time_start)
+print(time_end)
+
+while mamecas == True:
+    print("mamecas")
+
+    # !!! 1st photo name image0.jpg
+
+    if photo_number == 0:
+        photographer(photo_number)
+    else:
+        pass
+
+    photo_number = photo_number + 1
+
+    photographer(photo_number)
+
+    # comparison between latest and previous photo
+    picforcomp_1 = photo_number -1
+    picforcomp_2 = photo_number
+    measure_speed(picforcomp_1, picforcomp_2)
+
+    photo_number = photo_number + 1
+
+
+
+
+    #check for time
+    if time_start < time_end:
+        mamecas = True
+    else:
+        mamecas = False
+        break
+
+
+# konec
+print("program ukoncen")
+
+
