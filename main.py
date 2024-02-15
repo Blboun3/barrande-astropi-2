9f41b261ce9875e10ca36884b3a7584ce0335ff5
Blboun3 <62328614+Blboun3@users.noreply.github.com>
2024-02-15 07:50:46 +0100

    Fixed some details here and there
    - prepared comment blocks
    - fixed some logic
    - some typos, naming schemes etc.
    - wrote some comments
    - saving speed values to array

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+from measure_speed import measure_speed
 from photo_creation import photographer
 import time
 from txtfile_printer import printer
 import statistics
 
-#       start of the program
-
-#   variable declaration
-
 # variables for time keeping
 time_start = time.time()
-time_end = (time.time() + 570)
-mamecas = True
+time_end = (time.time() + 540)
+# true while we have time (i.e. runtime is less than 540 seconds (= 9 minutes))
+haveWeTime = True
 
 # file name numbering
 photo_number = 0
@@ -19,20 +17,24 @@ picforcomp_2 = None
 
 #average velocity calculation
 average_velocity = None
-velocity = []
 velocity_median = None
 deviation_max = None # median + 10%
 deviation_min = None # median - 10%
+
+# Array containing all measured velocities
+velocity = []
 
 # variable debugging
 print(time_start)
 print(time_end)
 
-while mamecas == True:
-    print("mamecas")
+# main loop
+while haveWeTime == True:
+    print("OK, we have time")
+
+    loop_start = time.time()
 
     # !!! 1st photo name image0.jpg
-
     if photo_number == 0:
         photographer(photo_number)
     else:
@@ -43,36 +45,37 @@ while mamecas == True:
     photographer(photo_number)
 
     # comparison between latest and previous photo
-    picforcomp_1 = photo_number -1
-    picforcomp_2 = photo_number
-    measure_speed(picforcomp_1, picforcomp_2)
+    picforcomp_1 = photo_number - 1 # previous photo
+    picforcomp_2 = photo_number # current photo
+    velocity.append(measure_speed(f'image{picforcomp_1}.jpg', f'image{picforcomp_2}.jpg'))
 
+    # 9 minutes runtime = 540 seconds
+    # limited to 42 photos
+    # 540 seconds divided by 42 photos  
+    # Average time per one cycle of this loop should be around 12 seconds
+    # current time - loop start time = how long has the loop ran for
+    # 12 - that = how long until 12 seconds
+    time.sleep(12 - (time.time() - loop_start))
 
-
-
-    #check for time
+    #check if we have time
     if time_start < time_end:
-        mamecas = True
+        haveWeTime = True
     else:
-        mamecas = False
+        haveWeTime = False
         break
 
 
 #calculating average speed
-
 velocity_median = statistics.median(velocity)
 print(velocity_median)
 
 deviation_max = velocity_median * 1.1
 deviation_min = velocity_median * 0.9
 
-
-
-
 # creates file 'report.txt' containing final resault
 printer(average_velocity)
 
-# konec
-print("program ended /n report containing outcome of our measuring can be found in report.txt")
+# End
+print("program ended /nReport containing outcome of our measuring can be found in report.txt")
 
 
--- photo_creation.py
+++ photo_creation.py
@@ -2,6 +2,11 @@ from picamera import PiCamera
 from time import sleep
 
 def photographer(photo_number):
+    """_summary_
+
+    Args:
+        photo_number (_type_): _description_
+    """
     print("photo_creation.py is starting")
 
     camera = PiCamera()
@@ -10,8 +15,8 @@ def photographer(photo_number):
 
     camera.start_preview()
 
-    # loop for capturing 42 photos at 10 sec intervals
-
+    # Wait for 4 seconds
+    # For camera to properly focus/adjust to light etc.
     sleep(4)
 
     camera.capture('image%s.jpg' % photo_number)
